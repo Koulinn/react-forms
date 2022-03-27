752f8056998fc75dd9263d9033532e38dd0fe013
Rafael Lima <drdverzola@gmail.com>
2022-03-27 14:25:54 +0100

    form completed

--- src/App.js
+++ src/App.js
@@ -1,4 +1,25 @@
+import { useState } from "react";
+
 function App() {
+  const [formValues, setFormValues] = useState({
+    name: "",
+    email: "",
+    password: "",
+    gender: "",
+    mailbox: false,
+  });
+
+  const { name, email, password, gender, mailbox } = formValues;
+
+  const handleFormValues = (value, formProperty) => {
+    setFormValues({ ...formValues, [formProperty]: value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("inside handle submit");
+  };
+
   return (
     <div className="App">
       <header className="App-header"></header>
@@ -11,16 +32,38 @@ function App() {
           padding: "2rem",
           maxWidth: "328px",
         }}
+        onSubmit={handleSubmit}
       >
         <label htmlFor="name">Name</label>
-        <input id="name" type="text"></input>
+        <input
+          id="name"
+          type="text"
+          value={name}
+          onChange={(e) => handleFormValues(e.target.value, "name")}
+        ></input>
+
         <label htmlFor="email">E-mail</label>
-        <input id="email" type="email"></input>
+        <input
+          id="email"
+          type="email"
+          value={email}
+          onChange={(e) => handleFormValues(e.target.value, "email")}
+        ></input>
+
         <label htmlFor="password">Password</label>
-        <input id="password" type="password"></input>
+        <input
+          id="password"
+          type="password"
+          value={password}
+          onChange={(e) => handleFormValues(e.target.value, "password")}
+        ></input>
 
         <label htmlFor="gender">Gender</label>
-        <select id="gender">
+        <select
+          id="gender"
+          value={gender}
+          onChange={(e) => handleFormValues(e.target.value, "gender")}
+        >
           <option>Male</option>
           <option>Female</option>
           <option>Other</option>
@@ -29,7 +72,15 @@ function App() {
 
         <label htmlFor="mailboxYes">
           Receive offers
-          <input id="mailbox" type="checkbox" />
+          <input
+            id="mailbox"
+            type="checkbox"
+            value={mailbox}
+            onChange={(e) => {
+              console.log(e);
+              handleFormValues(e.target.checked, "mailbox");
+            }}
+          />
         </label>
 
         <button type="submit"> Sign up!</button>
